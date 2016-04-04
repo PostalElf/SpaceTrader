6854eeca90266b913dfa07a7d232a4a390a75f54
PostalElf <thanthos@gmail.com>
2016-04-05 03:05:40 +0800

    Modified star.xy generation
    xy is now built from shared list.

--- SpaceTrader/World/star.vb
+++ SpaceTrader/World/star.vb
@@ -4,7 +4,8 @@
         With star
             ._starmap = starmap
             ._name = getRandomAndRemove(star.starNames, "data/starnames.txt", r)
-            .xy = New xy(r.Next(1, maxX), r.Next(1, maxY))
+            .xy = buildXy(r)
+            .imgString = buildImgString(r)
 
             Dim factionPair As faction() = starmap.getFactionPair(r)
             For n = 1 To r.Next(numPlanetsMin, numPlanetsMax + 1)
@@ -14,9 +15,39 @@
         End With
         Return star
     End Function
+    Private Shared Function buildXy(ByRef r As Random) As xy
+        If xys.Count = 0 Then
+            Dim xRange As Integer = (maxX / xySize) - 1
+            Dim yRange As Integer = (maxY / xySize) - 1
+
+            For x = 1 To xRange
+                For y = 1 To yRange
+                    xys.Add(New xy(x * xySize, y * xySize))
+                Next
+            Next
+        End If
+
+        Dim roll As Integer = r.Next(xys.Count - 1)
+        buildXy = xys(roll)
+        xys.RemoveAt(roll)
+    End Function
+    Private Shared Function buildImgString(ByRef r As Random) As String
+        Select Case r.Next(1, 6)
+            Case 1 : Return "Blue"
+            Case 2 : Return "BlueWhite"
+            Case 3 : Return "OrangeRed"
+            Case 4 : Return "Red"
+            Case 5 : Return "WhiteYellow"
+            Case Else
+                MsgBox("Error: buildImgString out of bounds.")
+                Return Nothing
+        End Select
+    End Function
+    Private Shared xys As New List(Of xy)
     Private Shared starNames As New List(Of String)
-    Friend Const maxX As Integer = 100
-    Friend Const maxY As Integer = 100
+    Friend Const maxX As Integer = 650
+    Friend Const maxY As Integer = 450
+    Friend Const xySize As Integer = 30
 
     Public Overrides Function ToString() As String
         Return name
@@ -31,18 +62,20 @@
         Next
     End Sub
 
-    Private _name As String
-    Friend ReadOnly Property name As String
-        Get
-            Return _name
-        End Get
-    End Property
     Private _starmap As starmap
     Friend ReadOnly Property starmap As starmap
         Get
             Return _starmap
         End Get
     End Property
+    Private _name As String
+    Friend ReadOnly Property name As String
+        Get
+            Return _name
+        End Get
+    End Property
+    Friend imgString As String
+
     Private _planets As New List(Of planet)
     Friend ReadOnly Property planets As List(Of planet)
         Get
